71a4fbc1af219da5fad821b3711088a1f6199847
L4nsir3x <122044456@upq.edu.mx>
2025-06-10 17:13:26 -0600

    Inicio practica 4

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,30 @@
+/*  Zona 1: Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+
+const Texto = (props)=>{
+  const {contenido}=props;
+  return(
+    <Text>{contenido}</Text>
+  )
+};
+
+
+/*  Zona 2: Main */
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola"></Texto>
+      <Button title='Trabaja'></Button>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="React Native"></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/*  Zona 3: Estilos del screen*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
