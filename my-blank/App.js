4dd1ede3dd9c8d576ffa0420cab095fdbc936e64
L4nsir3x <122044456@upq.edu.mx>
2025-07-01 17:09:19 -0600

    Practica 13

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,77 +1,98 @@
-import { View, Text, ActivityIndicator, Button, StyleSheet } from "react-native";
+import { SafeAreaView,
+   View, 
+   Text, 
+   FlatList,
+   SectionList,
+   StyleSheet,
+   ActivityIndicator   } from "react-native-web";
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [loading, setLoading] = useState(false);
-  const [mensaje, setMensaje] = useState('');
+  const[frutas, setFrutas] = useState([]);
+  const[verduras, setVerduras] = useState([]);
+  const[loading, setLoading] = useState(true);
+  const API_URL = "http://127.0.0.1:8000/productos/"; // Reemplaza con tu URL de API real
 
-const simularCarga = () => {
-  setLoading(true);
-  setMensaje('');
-  setTimeout(() =>{
-    setLoading(false);
-    setMensaje('Carga completada exitosamente');
-  }, 2000);
-}
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      setFrutas(data.frutas);
+      setVerduras(data.verduras);
+    })
+    .catch((error) => console.error("Error fetching data:", error))
+    .finally(() => setLoading(false));
+  }, []);
 
-return(
-  <View style={styles.container}>
-    <Text style = {styles.title}>
-      Simular carga
+  const  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.nombres}>{item.nombre}</Text>
+    </View>
+  );
+  const sections = [
+    { title: 'Frutas', data: frutas },
+    { title: 'Verduras', data: verduras },
+  ];
 
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={styles.texto}>Cargando...</Text>
+      </SafeAreaView>
+    );
+  }
 
-    </Text>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Lista de Frutas</Text>
+      <FlatList
+        data={frutas}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
 
-    {loading ? (
-      <>
-      <ActivityIndicator size="large" color="#0000ff" />
-      <Text style = {styles.texto}>Cargando...</Text>
-      </>
-      ) : (
-        <>
-        <Button title="Iniciar Carga" onPress={simularCarga}></Button>
-        {mensaje ? <Text style={styles.texto}>{mensaje}</Text> : null}
-        </>)}
+      <Text style={styles.title}>Frutas y Verduras{SectionList}</Text>
+      <SectionList
+        sections={sections}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
 
-  </View>
-)
 }
 
 /* Estilos */
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  texto: {
-    fontSize: 20,
-    color: 'black',
-    marginTop: 10,
-  },
-  overlay:{
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-
-  },
   container: {
     flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.5)', // overlay semitransparente
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 30,
+    paddingHorizontal: 16,
   },
   title: {
-    color: 'white',
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: 22,
+    fontWeight: "bold",
+    marginVertical: 12,
   },
-  subtitle: {
-    color: 'white',
-    fontSize: 18,
-  }
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    backgroundColor: "#ddd",
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 10,
+    marginVertical: 4,
+    borderRadius: 5,
+  },
+  nombre: {
+    fontSize: 18,
+  },
 });
